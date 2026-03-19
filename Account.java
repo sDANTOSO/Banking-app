332e7844333a2b440aacc18c564e1d3edb5f97ef
sDANTOSO <winatsa@whs.school.nz>
2026-03-19 14:34:09 +1300

    added stuff

---
+++ Account.java
@@ -0,0 +1,62 @@
+
+/**
+ * This is a constructor that will construct and edit peoples accounts
+ *
+ * @Santoso Winatan
+ * @19/03/26
+ */
+
+public class Account
+{
+    
+    private String customerName;
+    private int accountNumber;// how to hyphenate or should i concatenate c
+    //create error prevention system
+    private String customerAdress;
+    private AccountType accountType;
+    private float accountBalance;  
+    
+    public Account(){
+        
+        this.customerName=customerName;
+        this.accountNumber=accountNumber;
+        this.customerAdress=customerAdress;
+        this.accountType=accountType;
+        this.accountBalance=accountBalance;
+    }
+    
+    public void setcustomerName(String newValue){
+        this.customerName = newValue;
+    }
+    
+    public String getcustomerName(){
+        return(this.customerName);
+    }
+    
+    // add in account number
+    
+    public void setcustomerAdress(String newValue){
+        this.customerAdress = newValue;
+    }
+    
+    public String getcustomerAdress(){
+        return(this.customerAdress);
+    }
+    
+    public void setaccountType(AccountType newValue){
+        this.accountType = newValue;
+    }
+    
+    public AccountType getaccountType(){
+        return(this.accountType);
+    }
+    
+     public void setaccountBalance(float newValue){
+        this.accountBalance = newValue;
+    }
+    
+    public float getaccountBalance(){
+        return(this.accountBalance);
+    }
+    
+}
---
+++ AccountType.java
@@ -0,0 +1,13 @@
+
+/**
+ * This enum defines the different account types 
+ * this makes it easier to know that they are the same account type as 
+ * it will give an error if the do not match the three specific types
+ *
+ * @Santoso Winatan
+ * @19/03/26
+ */
+public enum AccountType
+{
+    EVERYDAY, SAVINGS, CURRENT
+}
